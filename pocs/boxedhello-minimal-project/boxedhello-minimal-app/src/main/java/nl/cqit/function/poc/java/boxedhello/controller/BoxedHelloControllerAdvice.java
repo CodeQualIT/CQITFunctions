4a05ece1e99b8e28f568d1cdf414965dee209d19
CC007 <Coolcat_the_best2@Hotmail.com>
2024-05-05 01:09:08 +0200

    Improve error handling

--- pocs/boxedhello-minimal-project/boxedhello-minimal-app/src/main/java/nl/cqit/function/poc/java/boxedhello/controller/BoxedHelloControllerAdvice.java
+++ pocs/boxedhello-minimal-project/boxedhello-minimal-app/src/main/java/nl/cqit/function/poc/java/boxedhello/controller/BoxedHelloControllerAdvice.java
@@ -1,5 +1,7 @@
 package nl.cqit.function.poc.java.boxedhello.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.boot.json.JsonParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -13,29 +15,28 @@ import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @ControllerAdvice
 public class BoxedHelloControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ProblemDetail handleRuntimeException(RuntimeException ex, WebRequest request) {
-
+        log.error("Unhandled exception", ex);
         if (ex instanceof JsonParseException) {
-            ProblemDetail body = getExceptionProblemDetail(
+            return getExceptionProblemDetail(
                     HttpStatus.BAD_REQUEST,
                     "/errors/validation-failed",
                     "Validation failed",
                     ex
             );
-            return body;
         }
 
-        ProblemDetail body = getExceptionProblemDetail(
+        return getExceptionProblemDetail(
                 HttpStatus.INTERNAL_SERVER_ERROR,
                 "/errors/unhandled-exception",
                 "Encountered an unhandled exception",
                 ex
         );
-        return body;
     }
 
     private ProblemDetail getExceptionProblemDetail(
@@ -48,19 +49,20 @@ public class BoxedHelloControllerAdvice extends ResponseEntityExceptionHandler {
         body.setType(URI.create(problemType));
         body.setTitle(problemDetail);
         body.setProperty("exceptionType", ex.getClass().getName());
+        body.setProperty("stacktrace", ExceptionUtils.getStackTrace(ex));
         if (ex.getCause() != null && ex.getCause() != ex) {
-            body.setProperty("cause", getCauseMap(ex, ex.getCause()));
+            body.setProperty("cause", getCauseMap(ex.getCause()));
         }
         return body;
     }
 
-    private Map<String, Object> getCauseMap(Throwable ex, Throwable cause) {
+    private Map<String, Object> getCauseMap(Throwable cause) {
         Map<String, Object> causeMap = new HashMap<>();
         causeMap.put("exceptionType", cause.getClass().getName());
         causeMap.put("message", cause.getLocalizedMessage());
-        causeMap.put("stacktrace", cause.getStackTrace());
+        causeMap.put("stacktrace", ExceptionUtils.getStackTrace(cause));
         if (cause.getCause() != null && cause.getCause() != cause) {
-            causeMap.put("cause", getCauseMap(cause, cause.getCause()));
+            causeMap.put("cause", getCauseMap(cause.getCause()));
         }
         return Map.copyOf(causeMap);
     }
